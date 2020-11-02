f4c5e8153ad2080c234051da24e3c1483b941905
riotchdhry <rohittchoudhary@outlook.com>
2020-11-01 21:40:39 -0600

    second commit-01-11:trying to deploy

--- mysqlConn.php
+++ mysqlConn.php
@@ -1,9 +1,9 @@
 <?php
 
-define('DB_USER','root');
-define('DB_PASSWORD','');
-define('DB_HOST', 'localhost:3307');
-define('DB_NAME', 'bookstore');
+define('DB_USER','sql205.epizy.com');
+define('DB_PASSWORD','AYb5iGiZXqs3');
+define('DB_HOST', 'epiz_27103686');
+define('DB_NAME', 'epiz_27103686_project1');
 
 
 $dbc= mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_NAME) OR die('could not connect to Mysqli:'.mysqli_connect_error());
